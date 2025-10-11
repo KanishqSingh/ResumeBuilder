597ba012bf42f37a10705bbf649a8faa27e30ea0
Kanishq SIngh <singhkanishq06@gmail.com>
2025-10-11 18:24:48 +0530

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    const user = {name:"John Doe"}
+  return (
+    <div className='shadow bg-white'>
+        <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3 5 text-slate-800 transition-all">
+
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
